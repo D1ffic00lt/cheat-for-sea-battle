064e56861454fae3fde89961b548e2afb05d05c2
D1ffic00lt <dm.filinov@gmail.com>
2023-12-25 19:05:56 +0700

    add board simulation

--- cheat/gamecalculator/board.py
+++ cheat/gamecalculator/board.py
@@ -1,8 +1,11 @@
+from .field import Field
 
 
 class Board(object):
     def __init__(self):
         self.__board = []
+        for _ in range(10):
+            self.__board.append([Field() for _ in range(10)])
 
     @property
     def board(self):
---
+++ cheat/gamecalculator/field.py
@@ -0,0 +1,13 @@
+
+
+class Field(object):
+    def __init__(self, status=True, shoot=False, ship=False):
+        self.status = status
+        self.shoot = shoot
+        self.ship = ship
+
+    def __str__(self):
+        return f"{self.__class__.__name__}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.status}, {self.shoot}, {self.ship})"
