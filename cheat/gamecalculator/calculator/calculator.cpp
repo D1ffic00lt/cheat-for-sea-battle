a5f478a281619e7d2c5bd8301a811332cfec28f3
D1ffic00lt <dm.filinov@gmail.com>
2023-12-29 12:14:16 +0700

    names fix

--- cheat/gamecalculator/calculator/calculator.cpp
+++ cheat/gamecalculator/calculator/calculator.cpp
@@ -2,59 +2,59 @@
 
 std::vector<std::vector<int>> Calculator::calculate() {
     for (int y = 0; y < 10; y++) {
-        this->probability_board.emplace_back();
+        this->probabilityBoard.emplace_back();
         for (int x = 0; x < 10; x++) {
-            if (this->single_decker_ships_count > 0) {
-                this->probability_board[y].push_back(1);
+            if (this->singleDeckerShipsCount > 0) {
+                this->probabilityBoard[y].push_back(1);
             }
             else {
-                this->probability_board[y].push_back(0);
+                this->probabilityBoard[y].push_back(0);
             }
         }
     }
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
             if ((this->board[y][x].ship) and (not this->board[y][x].shooted)) {
-                this->probability_board[y][x] = 0;
+                this->probabilityBoard[y][x] = 0;
                 this->board[y][x].status = false;
 
                 if (y != 9) {
-                    this->probability_board[y + 1][x] = 100;
+                    this->probabilityBoard[y + 1][x] = 100;
                 }
 
                 if (y != 0) {
-                    this->probability_board[y - 1][x] = 100;
+                    this->probabilityBoard[y - 1][x] = 100;
                 }
 
                 if (x != 9) {
-                    this->probability_board[y][x + 1] = 100;
+                    this->probabilityBoard[y][x + 1] = 100;
                 }
 
                 if (x != 0) {
-                    this->probability_board[y][x - 1] = 100;
+                    this->probabilityBoard[y][x - 1] = 100;
                 }
 
             }
             if (this->board[y][x].shooted) {
-                this->probability_board[y][x] = 0;
+                this->probabilityBoard[y][x] = 0;
                 this->board[y][x].status = false;
                 if (y != 9) {
-                    this->probability_board[y + 1][x] = 0;
+                    this->probabilityBoard[y + 1][x] = 0;
                     this->board[y + 1][x].status = false;
                 }
 
                 if (y != 0) {
-                    this->probability_board[y - 1][x] = 0;
+                    this->probabilityBoard[y - 1][x] = 0;
                     this->board[y - 1][x].status = false;
                 }
 
                 if (x != 9) {
-                    this->probability_board[y][x + 1] = 0;
+                    this->probabilityBoard[y][x + 1] = 0;
                     this->board[y][x + 1].status = false;
                 }
 
                 if (x != 0) {
-                    this->probability_board[y][x - 1] = 0;
+                    this->probabilityBoard[y][x - 1] = 0;
                     this->board[y][x - 1].status = false;
                 }
 
@@ -64,61 +64,61 @@ std::vector<std::vector<int>> Calculator::calculate() {
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
             if (not this->board[y][x].status) {
-                this->probability_board[y][x] = 0;
+                this->probabilityBoard[y][x] = 0;
                 continue;
             }
 
-            if (this->double_decker_ships_count > 0) {
+            if (this->doubleDeckerShipsCount > 0) {
                 if (x < 9) {
-                    current_data = {};
+                    currentData = {};
                     for (int x1 = x; x1 < x + 2; x1++) {
-                        current_data.push_back(this->board[y][x1]);
+                        currentData.push_back(this->board[y][x1]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 2; i++) {
-                            this->probability_board[y][x + i] += 1;
+                            this->probabilityBoard[y][x + i] += 1;
                         }
                     }
                 }
 
                 if (y < 9) {
-                    current_data = {};
+                    currentData = {};
                     for (int y1 = y; y1 < y + 2; y1++) {
-                        current_data.push_back(this->board[y1][x]);
+                        currentData.push_back(this->board[y1][x]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 2; i++) {
-                            this->probability_board[y + i][x] += 1;
+                            this->probabilityBoard[y + i][x] += 1;
                         }
                     }
                 }
             }
 
-            if (this->three_decker_ships_count > 0) {
+            if (this->threeDeckerShipsCount > 0) {
                 if (x < 8) {
-                    current_data = {};
+                    currentData = {};
                     for (int x1 = x; x1 < x + 3; x1++) {
-                        current_data.push_back(this->board[y][x1]);
+                        currentData.push_back(this->board[y][x1]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 3; i++) {
-                            this->probability_board[y][x + i] += 1;
+                            this->probabilityBoard[y][x + i] += 1;
                         }
                     }
                 }
 
                 if (y < 8) {
-                    current_data = {};
+                    currentData = {};
                     for (int y1 = y; y1 < y + 2; y1++) {
-                        current_data.push_back(this->board[y1][x]);
+                        currentData.push_back(this->board[y1][x]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 3; i++) {
-                            this->probability_board[y + i][x] += 1;
+                            this->probabilityBoard[y + i][x] += 1;
                         }
                     }
                 }
@@ -126,53 +126,53 @@ std::vector<std::vector<int>> Calculator::calculate() {
             }
 
 
-            if (this->four_decker_ships_count > 0) {
+            if (this->fourDeckerShipsCount > 0) {
                 if (x < 7) {
-                    current_data = {};
+                    currentData = {};
                     for (int x1 = x; x1 < x + 4; x1++) {
-                        current_data.push_back(this->board[y][x1]);
+                        currentData.push_back(this->board[y][x1]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 4; i++) {
-                            this->probability_board[y][x + i] += 1;
+                            this->probabilityBoard[y][x + i] += 1;
                         }
                     }
                 }
 
                 if (y < 7) {
-                    current_data = {};
+                    currentData = {};
                     for (int y1 = y; y1 < y + 4; y1++) {
-                        current_data.push_back(this->board[y1][x]);
+                        currentData.push_back(this->board[y1][x]);
                     }
 
-                    if (Calculator::checkFields(current_data)) {
+                    if (Calculator::checkFields(currentData)) {
                         for (int i = 0; i < 4; i++) {
-                            this->probability_board[y + i][x] += 1;
+                            this->probabilityBoard[y + i][x] += 1;
                         }
                     }
                 }
             }
         }
     }
-    return this->probability_board;
+    return this->probabilityBoard;
 }
 
-std::pair<int, int> Calculator::get_best_move() {
-    if (this->probability_board.empty()) {
+[[maybe_unused]] std::pair<int, int> Calculator::getBestMove() {
+    if (this->probabilityBoard.empty()) {
         this->calculate();
     }
     int best_move = -1;
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
-            if (best_move < this->probability_board[y][x]) {
-                best_move = this->probability_board[y][x];
+            if (best_move < this->probabilityBoard[y][x]) {
+                best_move = this->probabilityBoard[y][x];
             }
         }
     }
     for (int y = 0; y < 10; y++) {
         for (int x = 0; x < 10; x++) {
-            if (this->probability_board[y][x] == best_move) {
+            if (this->probabilityBoard[y][x] == best_move) {
                 return std::make_pair(y, x);
             }
         }
@@ -180,41 +180,41 @@ std::pair<int, int> Calculator::get_best_move() {
     return std::make_pair(0, 0);
 }
 
-bool Calculator::checkFields(std::vector<Field> &fields) {
+[[maybe_unused]] bool Calculator::checkFields(std::vector<Field> &fields) {
     return std::all_of(fields.begin(), fields.end(), [](const Field &field) {
         return field.status;
     });
 }
 
-void Calculator::set_ship(int x, int y, bool value) {
+[[maybe_unused]] void Calculator::setShip(int x, int y, bool value) {
     this->board[y][x].ship = value;
 }
 
-void Calculator::set_shooted(int x, int y, bool value) {
+[[maybe_unused]] void Calculator::setShooted(int x, int y, bool value) {
     this->board[y][x].shooted = value;
 }
 
-void Calculator::set_status(int x, int y, bool value) {
+[[maybe_unused]] void Calculator::setStatus(int x, int y, bool value) {
     this->board[y][x].status = value;
 }
 
-void Calculator::remove_single_decker_ship() {
-    if (this->single_decker_ships_count > 0) {
-        this->single_decker_ships_count--;
+[[maybe_unused]] void Calculator::removeSingleDeckerShip() {
+    if (this->singleDeckerShipsCount > 0) {
+        this->singleDeckerShipsCount--;
     }
 }
-void Calculator::remove_double_decker_ship() {
-    if (this->double_decker_ships_count > 0) {
-        this->double_decker_ships_count--;
+[[maybe_unused]] void Calculator::removeDoubleDeckerShip() {
+    if (this->doubleDeckerShipsCount > 0) {
+        this->doubleDeckerShipsCount--;
     }
 }
-void Calculator::three_single_decker_ship() {
-    if (this->three_decker_ships_count > 0) {
-        this->three_decker_ships_count--;
+[[maybe_unused]] void Calculator::threeSingleDeckerShip() {
+    if (this->threeDeckerShipsCount > 0) {
+        this->threeDeckerShipsCount--;
     }
 }
-void Calculator::four_single_decker_ship() {
-    if (this->four_decker_ships_count > 0) {
-        this->four_decker_ships_count--;
+[[maybe_unused]] void Calculator::fourSingleDeckerShip() {
+    if (this->fourDeckerShipsCount > 0) {
+        this->fourDeckerShipsCount--;
     }
 }
--- cheat/gamecalculator/calculator/calculator.h
+++ cheat/gamecalculator/calculator/calculator.h
@@ -11,28 +11,28 @@ class [[maybe_unused]] Calculator : Board {
 private:
     static bool checkFields(std::vector<Field> &fields);
 
-    int single_decker_ships_count = 0, double_decker_ships_count = 3, three_decker_ships_count = 2, four_decker_ships_count = 1;
+    int singleDeckerShipsCount = 0, doubleDeckerShipsCount = 3, threeDeckerShipsCount = 2, fourDeckerShipsCount = 1;
 
-    std::vector<std::vector<int>> probability_board = std::vector<std::vector<int>>();
-    std::vector<Field> current_data;
+    std::vector<std::vector<int>> probabilityBoard = std::vector<std::vector<int>>();
+    std::vector<Field> currentData;
 public:
     explicit Calculator(int single_decker_ships_count = 0, int double_decker_ships_count = 3, int three_decker_ships_count = 2, int four_decker_ships_count = 1) : Board() {
-        this->single_decker_ships_count = single_decker_ships_count;
-        this->double_decker_ships_count = double_decker_ships_count;
-        this->three_decker_ships_count = three_decker_ships_count;
-        this->four_decker_ships_count = four_decker_ships_count;
+        this->singleDeckerShipsCount = single_decker_ships_count;
+        this->doubleDeckerShipsCount = double_decker_ships_count;
+        this->threeDeckerShipsCount = three_decker_ships_count;
+        this->fourDeckerShipsCount = four_decker_ships_count;
     };
 
     [[maybe_unused]] std::vector<std::vector<int>> calculate();
-    [[maybe_unused]] std::pair<int, int> get_best_move();
-    [[maybe_unused]] void set_ship(int x, int y, bool value);
-    [[maybe_unused]] void set_shooted(int x, int y, bool value);
-    [[maybe_unused]] void set_status(int x, int y, bool value);
+    [[maybe_unused]] std::pair<int, int> getBestMove();
+    [[maybe_unused]] void setShip(int x, int y, bool value);
+    [[maybe_unused]] void setShooted(int x, int y, bool value);
+    [[maybe_unused]] void setStatus(int x, int y, bool value);
 
-    [[maybe_unused]] void remove_single_decker_ship();
-    [[maybe_unused]] void remove_double_decker_ship();
-    [[maybe_unused]] void three_single_decker_ship();
-    [[maybe_unused]] void four_single_decker_ship();
+    [[maybe_unused]] void removeSingleDeckerShip();
+    [[maybe_unused]] void removeDoubleDeckerShip();
+    [[maybe_unused]] void threeSingleDeckerShip();
+    [[maybe_unused]] void fourSingleDeckerShip();
 };
 
 #endif
